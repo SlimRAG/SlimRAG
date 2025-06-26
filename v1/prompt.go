6156a567cec0918fdd1420632761c38713feab8d
fanyang <fanyang@smartx.com>
2025-06-26 13:24:47 +0800

    Force using Chinese

--- v1/prompt.go
+++ v1/prompt.go
@@ -7,7 +7,7 @@ import (
 
 func buildPrompt(query string, documents []DocumentChunk) string {
 	var b strings.Builder
-	b.WriteString("根据以下知识回答问题：\n\n")
+	b.WriteString("根据以下知识，使用中文回答问题：\n\n")
 	for i, doc := range documents {
 		b.WriteString(fmt.Sprintf("知识片段 %d：%s\n\n", i, doc.Text))
 	}
